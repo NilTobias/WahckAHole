9364967f57423e885f9fc62e21eecf162d6b51b7
NilTobias <ryufiguracion@yahoo.com.sg>
2020-10-31 11:50:56 +0800

    Initial commit
    yes

---
+++ WhackAHole/Assets/Scripts/Mole/Buff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff
+{
+    public int Value;
+    public BuffType Type;
+    public Buff(BuffType type, int value)
+    {
+
+    }
+}
+public enum BuffType
+{
+    Heal,
+    Speed,
+    Power,
+    Shield,
+}
---
+++ WhackAHole/Assets/Scripts/Mole/Mole.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+    [Header("Stats")]
+    public int Health;
+    public int AttackPower;
+    public float AttackSpeed;
+    public int Shield;
+
+    private int HealthBuff;
+    private int AttackPowerBuff;
+    private float AttackSpeedBuff;
+
+    public void ReceiveDamage(int damage)
+    {
+    }
+    public void ReceiveBuff(Buff buff)
+    {
+    }
+}
